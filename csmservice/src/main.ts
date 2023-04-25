23777a7a049d3a871c47b988614beb034e51dc4d
Stijn van Houwelingen <stijn@van-houwelingen.net>
2023-04-25 14:17:30 +0200

    added first service

---
+++ csmservice/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+//TODO - change queue name
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'name_here',
+      queueOptions: {
+        durable: false
+      },
+    },
+  });
+  
+  await app.listen();
+}
+bootstrap();
