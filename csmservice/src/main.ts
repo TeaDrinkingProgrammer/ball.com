636ef8b57f6eb1840836c2e42ab5c1514c6f26ac
Stijn van Houwelingen <stijn@van-houwelingen.net>
2023-05-22 10:34:46 +0200

    Nest setup

--- csmservice/src/main.ts
+++ csmservice/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://localhost:5672'],
+      urls: ['amqp://guest:guest@127.0.0.1:5672'],
       queue: 'name_here',
       queueOptions: {
         durable: false
