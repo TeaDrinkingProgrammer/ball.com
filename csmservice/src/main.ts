05f8c0e2d1e79b72080820e193ccba268acf6271
Stijn van Houwelingen <stijn@van-houwelingen.net>
2023-05-22 11:48:17 +0200

    refactored docker compose and csmservice

--- csmservice/src/main.ts
+++ csmservice/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { log } from 'console';
 
 //TODO - change queue name
 async function bootstrap() {
+  console.log("Starting microservice")
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://guest:guest@127.0.0.1:5672'],
+      urls: ['amqp://guest:guest@rabbitmq:5672'],
       queue: 'name_here',
       queueOptions: {
         durable: false
