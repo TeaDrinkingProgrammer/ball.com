e24e74a18d002a348027b714d830f7fd790c10a5
Stijn van Houwelingen <stijn@van-houwelingen.net>
2023-05-31 12:58:03 +0200

    fixed order placed event (#34)

--- InventoryManagement/src/product-event.controller.ts
+++ InventoryManagement/src/product-event.controller.ts
@@ -1,18 +1,26 @@
-import { Logger } from "@nestjs/common";
+import { Controller, Logger } from "@nestjs/common";
 import { EventPattern } from "@nestjs/microservices";
 import { ProductService } from "./product.service";
 import { ProductStockPayload } from "./models/product";
 
+@Controller('product')
 export class ProductEventController {
     constructor(private readonly productService: ProductService) { }
-    @EventPattern('ProductStockChanged')
+    @EventPattern('OrderPlaced')
     async updateProductStockEvent(data: Record<string, unknown>) {
-      Logger.log('Product created', data);
+      Logger.log('Order placed', data);
       const jsonObject = JSON.parse(JSON.stringify(data));
-      try {
-        await this.productService.updateProductStock(jsonObject as ProductStockPayload);
-      } catch (error) {
-        return { message: error, status: 400 };
+      
+      for (const product of jsonObject.products) {
+        const productStockPayload: ProductStockPayload = {
+          id: product.product.productId,
+          quantity: product.quantity
+        }
+        try {
+          await this.productService.orderPlaced(productStockPayload);
+        } catch (error) {
+          return { message: error, status: 400 };
+        }
       }
     }
 }
